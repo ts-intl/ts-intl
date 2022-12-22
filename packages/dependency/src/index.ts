77f18be34654f33551b035ffabb8fdfcbcc1166a
Hakon Liang <hakon.liang@crypto.com>
2022-12-22 17:20:28 +0800

    fix: dependency missing export

--- packages/dependency/src/index.ts
+++ packages/dependency/src/index.ts
@@ -1,5 +1,6 @@
 export * from './compiler/extractIntlKeysFromCode';
 export * from './getDependencies';
 export * from './getDependenciesByEntries';
+export * from './getDependenciesFs';
 export * from './types';
 export * from './utils';
